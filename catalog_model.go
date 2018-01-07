ef5339f9aa517647b2572265c6ce490837ad01de
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2018-01-07 17:03:55 -0300

    fix

--- catalog_model.go
+++ catalog_model.go
@@ -31,12 +31,18 @@ var (
 	mvd = CityConfig{
 		Name: "Montevideo",
 		Code: "MVD",
-		Lat:  -34.961772,
-		Lng:  -54.942884,
+		Lat:  -34.901113,
+		Lng:  -56.164531,
 		Neighbourhoods: []NameCode{
 			NameCode{Code: "AGUADA", Name: "Aguada", Lat: -34.883501, Lng: -56.190684},
 			NameCode{Code: "POCITOS", Name: "Pocitos", Lat: -34.908530, Lng: -56.150406}}}
-	pde = CityConfig{Name: "Punta del Este", Code: "PDE", Neighbourhoods: []NameCode{}}
+	pde = CityConfig{
+		Name:           "Punta del Este",
+		Code:           "PDE",
+		Lat:            -34.961772,
+		Lng:            -54.942884,
+		Neighbourhoods: []NameCode{},
+	}
 )
 
 var CountryCitites = map[string]CountryConfig{uy.Code: uy, ar.Code: ar}
