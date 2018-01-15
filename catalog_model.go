54cc25c4996802d0c53676be6a46ca96f3e25b1c
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2018-01-15 20:08:53 -0300

    fixes

--- catalog_model.go
+++ catalog_model.go
@@ -1,9 +1,9 @@
 package m
 
 type CountryConfig struct {
-	Code       string
-	Cities     []CityConfig
-	Guarantees []string
+	Code          string
+	Cities        []CityConfig
+	PropertyTypes []NameCode
 }
 
 type CityConfig struct {
@@ -18,6 +18,7 @@ type CityConfig struct {
 type NameCode struct {
 	Name string
 	Code string
+	Type string
 	Lat  float64
 	Lng  float64
 }
@@ -50,141 +51,110 @@ var Amenities = map[string]struct{}{
 
 var CountryCitites = map[string]CountryConfig{uy.Code: uy, ar.Code: ar}
 
-// Agua Caliente Central
-// Aire Acondicionado
-// Altillo
-// Amueblada
-// Balcón
-// Barbacoa
-// Box
-// Bungalow
-// Calefacción
-// Calefón
-// Cochera
-// Depósito
-// Dormitorio de servicio
-// Estufa a Leña
-// Garaje
-// Gas por Cañería
-// GYM
-// Instalación de TV cable
-// Jacuzzi
-// Jardín
-// Lavadero
-// Lavandería
-// Línea Blanca
-// Losa Radiante
-// Parrillero
-// Patio
-// Piscina
-// Placard en cocina
-// Placard en dormitorio
-// Playroom
-// Previsión A.A.
-// Sauna
-// Sótano
-// Terraza
-// Terraza Lavadero
-// Vestidor
-// WiFi
-
-// Alarma
-// Cámaras CCTV
-// Cerca Perimetral
-// Portería 24 hrs
-// Portón Eléctrico
-// Rejas
-// Guardia de Seguridad
 var CountryAmenities = map[string][]NameCode{
-	ar.Code:[]NameCode{ 
-		NameCode{Code: "AGUA_CALIENTE_CENTRAL" , Name: "Agua Caliente Central"},
-			NameCode{Code: "AIRE_ACONDICIONADO" , Name: "Aire Acondicionado"},
-			NameCode{Code: "ALTILLO" , Name: "Altillo"},
-			NameCode{Code: "AMUEBLADA" , Name: "Amueblada"},
-			NameCode{Code: "BALCON" , Name: "Balcón"},
-			NameCode{Code: "BARBACOA" , Name: "Barbacoa"},
-			NameCode{Code: "BOX" , Name: "Box"},
-			NameCode{Code: "BUNGALOW" , Name: "Bungalow"},
-			NameCode{Code: "CALEFACCION" , Name: "Calefacción"},
-			NameCode{Code: "CALEFON" , Name: "Calefón"},
-			NameCode{Code: "COCHERA" , Name: "Cochera"},
-			NameCode{Code: "DEPOSITO" , Name: "Depósito"},
-			NameCode{Code: "DORMITORIO_DE_SERVICIO" , Name: "Dormitorio de servicio"},
-			NameCode{Code: "ESTUFA_LENA" , Name: "Estufa a Leña"},
-			NameCode{Code: "GARAGE" , Name: "Garaje"},
-			NameCode{Code: "GAS_CANERIA" , Name: "Gas por Cañería"},
-			NameCode{Code: "GYM" , Name: "GYM"},
-			NameCode{Code: "INSTALACION_TV_CABLE" , Name: "Instalación de TV cable"},
-			NameCode{Code: "JACUZZI" , Name: "Jacuzzi"},
-			NameCode{Code: "JARDIN" , Name: "Jardín"},
-			NameCode{Code: "LAVADERO" , Name: "Lavadero"},
-			NameCode{Code: "LAVANDERIA" , Name: "Lavandería"},
-			NameCode{Code: "LINEA_BLANCA" , Name: "Línea Blanca"},
-			NameCode{Code: "LOSA_RADIANTE" , Name: "Losa Radiante"},
-			NameCode{Code: "PARRILLERO" , Name: "Parrillero"},
-			NameCode{Code: "PATIO" , Name: "Patio"},
-			NameCode{Code: "PISCINA" , Name: "Piscina"},
-			NameCode{Code: "PLACARD_EN_COCINA" , Name: "Placard en cocina"},
-			NameCode{Code: "PLACARD_EN_DORMITORIO" , Name: "Placard en dormitorio"},
-			NameCode{Code: "PLAYROOM" , Name: "Playroom"},
-			NameCode{Code: "PREVISION_A_A" , Name: "Previsión A.A."},
-			NameCode{Code: "SAUNA" , Name: "Sauna"},
-			NameCode{Code: "SOTANO" , Name: "Sótano"},
-			NameCode{Code: "TERRAZA" , Name: "Terraza"},
-			NameCode{Code: "TERRAZA_LAVADERO" , Name: "Terraza Lavadero"},
-			NameCode{Code: "VESTIDOR" , Name: "Vestidor"},
-			NameCode{Code: "WIFI" , Name: "WiFi"}},
-		uy.Code: []NameCode{ 
-			NameCode{Code: "AGUA_CALIENTE_CENTRAL" , Name: "Agua Caliente Central"},
-			NameCode{Code: "AIRE_ACONDICIONADO" , Name: "Aire Acondicionado"},
-			NameCode{Code: "ALTILLO" , Name: "Altillo"},
-			NameCode{Code: "AMUEBLADA" , Name: "Amueblada"},
-			NameCode{Code: "BALCON" , Name: "Balcón"},
-			NameCode{Code: "BARBACOA" , Name: "Barbacoa"},
-			NameCode{Code: "BOX" , Name: "Box"},
-			NameCode{Code: "BUNGALOW" , Name: "Bungalow"},
-			NameCode{Code: "CALEFACCION" , Name: "Calefacción"},
-			NameCode{Code: "CALEFON" , Name: "Calefón"},
-			NameCode{Code: "COCHERA" , Name: "Cochera"},
-			NameCode{Code: "DEPOSITO" , Name: "Depósito"},
-			NameCode{Code: "DORMITORIO_DE_SERVICIO" , Name: "Dormitorio de servicio"},
-			NameCode{Code: "ESTUFA_LENA" , Name: "Estufa a Leña"},
-			NameCode{Code: "GARAGE" , Name: "Garaje"},
-			NameCode{Code: "GAS_CANERIA" , Name: "Gas por Cañería"},
-			NameCode{Code: "GYM" , Name: "GYM"},
-			NameCode{Code: "INSTALACION_TV_CABLE" , Name: "Instalación de TV cable"},
-			NameCode{Code: "JACUZZI" , Name: "Jacuzzi"},
-			NameCode{Code: "JARDIN" , Name: "Jardín"},
-			NameCode{Code: "LAVADERO" , Name: "Lavadero"},
-			NameCode{Code: "LAVANDERIA" , Name: "Lavandería"},
-			NameCode{Code: "LINEA_BLANCA" , Name: "Línea Blanca"},
-			NameCode{Code: "LOSA_RADIANTE" , Name: "Losa Radiante"},
-			NameCode{Code: "PARRILLERO" , Name: "Parrillero"},
-			NameCode{Code: "PATIO" , Name: "Patio"},
-			NameCode{Code: "PISCINA" , Name: "Piscina"},
-			NameCode{Code: "PLACARD_EN_COCINA" , Name: "Placard en cocina"},
-			NameCode{Code: "PLACARD_EN_DORMITORIO" , Name: "Placard en dormitorio"},
-			NameCode{Code: "PLAYROOM" , Name: "Playroom"},
-			NameCode{Code: "PREVISION_A_A" , Name: "Previsión A.A."},
-			NameCode{Code: "SAUNA" , Name: "Sauna"},
-			NameCode{Code: "SOTANO" , Name: "Sótano"},
-			NameCode{Code: "TERRAZA" , Name: "Terraza"},
-			NameCode{Code: "TERRAZA_LAVADERO" , Name: "Terraza Lavadero"},
-			NameCode{Code: "VESTIDOR" , Name: "Vestidor"},
-			NameCode{Code: "WIFI" , Name: "WiFi"}}}
+	ar.Code: []NameCode{
+		NameCode{Type: "SECURITY", Code: "ALARMA", Name: "Alarma"},
+		NameCode{Type: "SECURITY", Code: "CAMARA_CCTV", Name: "Cámaras CCTV"},
+		NameCode{Type: "SECURITY", Code: "CERCA_PERIMETRAL", Name: "Cerca Perimetral"},
+		NameCode{Type: "SECURITY", Code: "PORTERIA_24", Name: "Portería 24 hrs"},
+		NameCode{Type: "SECURITY", Code: "PORTON_ELECTRICO", Name: "Portón Eléctrico"},
+		NameCode{Type: "SECURITY", Code: "REJAS", Name: "Rejas"},
+		NameCode{Type: "SECURITY", Code: "GUARDIA_SEGURIDAD", Name: "Guardia de Seguridad"},
+		NameCode{Type: "OTHER", Code: "AGUA_CALIENTE_CENTRAL", Name: "Agua Caliente Central"},
+		NameCode{Type: "OTHER", Code: "AIRE_ACONDICIONADO", Name: "Aire Acondicionado"},
+		NameCode{Type: "OTHER", Code: "ALTILLO", Name: "Altillo"},
+		NameCode{Type: "OTHER", Code: "AMUEBLADA", Name: "Amueblada"},
+		NameCode{Type: "OTHER", Code: "BALCON", Name: "Balcón"},
+		NameCode{Type: "OTHER", Code: "BARBACOA", Name: "Barbacoa"},
+		NameCode{Type: "OTHER", Code: "BOX", Name: "Box"},
+		NameCode{Type: "OTHER", Code: "BUNGALOW", Name: "Bungalow"},
+		NameCode{Type: "OTHER", Code: "CALEFACCION", Name: "Calefacción"},
+		NameCode{Type: "OTHER", Code: "CALEFON", Name: "Calefón"},
+		NameCode{Type: "OTHER", Code: "COCHERA", Name: "Cochera"},
+		NameCode{Type: "OTHER", Code: "DEPOSITO", Name: "Depósito"},
+		NameCode{Type: "OTHER", Code: "DORMITORIO_DE_SERVICIO", Name: "Dormitorio de servicio"},
+		NameCode{Type: "OTHER", Code: "ESTUFA_LENA", Name: "Estufa a Leña"},
+		NameCode{Type: "OTHER", Code: "GARAGE", Name: "Garaje"},
+		NameCode{Type: "OTHER", Code: "GAS_CANERIA", Name: "Gas por Cañería"},
+		NameCode{Type: "OTHER", Code: "GYM", Name: "GYM"},
+		NameCode{Type: "OTHER", Code: "INSTALACION_TV_CABLE", Name: "Instalación de TV cable"},
+		NameCode{Type: "OTHER", Code: "JACUZZI", Name: "Jacuzzi"},
+		NameCode{Type: "OTHER", Code: "JARDIN", Name: "Jardín"},
+		NameCode{Type: "OTHER", Code: "LAVADERO", Name: "Lavadero"},
+		NameCode{Type: "OTHER", Code: "LAVANDERIA", Name: "Lavandería"},
+		NameCode{Type: "OTHER", Code: "LINEA_BLANCA", Name: "Línea Blanca"},
+		NameCode{Type: "OTHER", Code: "LOSA_RADIANTE", Name: "Losa Radiante"},
+		NameCode{Type: "OTHER", Code: "PARRILLERO", Name: "Parrillero"},
+		NameCode{Type: "OTHER", Code: "PATIO", Name: "Patio"},
+		NameCode{Type: "OTHER", Code: "PISCINA", Name: "Piscina"},
+		NameCode{Type: "OTHER", Code: "PLACARD_EN_COCINA", Name: "Placard en cocina"},
+		NameCode{Type: "OTHER", Code: "PLACARD_EN_DORMITORIO", Name: "Placard en dormitorio"},
+		NameCode{Type: "OTHER", Code: "PLAYROOM", Name: "Playroom"},
+		NameCode{Type: "OTHER", Code: "PREVISION_A_A", Name: "Previsión A.A."},
+		NameCode{Type: "OTHER", Code: "SAUNA", Name: "Sauna"},
+		NameCode{Type: "OTHER", Code: "SOTANO", Name: "Sótano"},
+		NameCode{Type: "OTHER", Code: "TERRAZA", Name: "Terraza"},
+		NameCode{Type: "OTHER", Code: "TERRAZA_LAVADERO", Name: "Terraza Lavadero"},
+		NameCode{Type: "OTHER", Code: "VESTIDOR", Name: "Vestidor"},
+		NameCode{Type: "OTHER", Code: "WIFI", Name: "WiFi"}},
+	uy.Code: []NameCode{
+		NameCode{Type: "SECURITY", Code: "ALARMA", Name: "Alarma"},
+		NameCode{Type: "SECURITY", Code: "CAMARA_CCTV", Name: "Cámaras CCTV"},
+		NameCode{Type: "SECURITY", Code: "CERCA_PERIMETRAL", Name: "Cerca Perimetral"},
+		NameCode{Type: "SECURITY", Code: "PORTERIA_24", Name: "Portería 24 hrs"},
+		NameCode{Type: "SECURITY", Code: "PORTON_ELECTRICO", Name: "Portón Eléctrico"},
+		NameCode{Type: "SECURITY", Code: "REJAS", Name: "Rejas"},
+		NameCode{Type: "SECURITY", Code: "GUARDIA_SEGURIDAD", Name: "Guardia de Seguridad"},
+		NameCode{Type: "OTHER", Code: "AGUA_CALIENTE_CENTRAL", Name: "Agua Caliente Central"},
+		NameCode{Type: "OTHER", Code: "AIRE_ACONDICIONADO", Name: "Aire Acondicionado"},
+		NameCode{Type: "OTHER", Code: "ALTILLO", Name: "Altillo"},
+		NameCode{Type: "OTHER", Code: "AMUEBLADA", Name: "Amueblada"},
+		NameCode{Type: "OTHER", Code: "BALCON", Name: "Balcón"},
+		NameCode{Type: "OTHER", Code: "BARBACOA", Name: "Barbacoa"},
+		NameCode{Type: "OTHER", Code: "BOX", Name: "Box"},
+		NameCode{Type: "OTHER", Code: "BUNGALOW", Name: "Bungalow"},
+		NameCode{Type: "OTHER", Code: "CALEFACCION", Name: "Calefacción"},
+		NameCode{Type: "OTHER", Code: "CALEFON", Name: "Calefón"},
+		NameCode{Type: "OTHER", Code: "COCHERA", Name: "Cochera"},
+		NameCode{Type: "OTHER", Code: "DEPOSITO", Name: "Depósito"},
+		NameCode{Type: "OTHER", Code: "DORMITORIO_DE_SERVICIO", Name: "Dormitorio de servicio"},
+		NameCode{Type: "OTHER", Code: "ESTUFA_LENA", Name: "Estufa a Leña"},
+		NameCode{Type: "OTHER", Code: "GARAGE", Name: "Garaje"},
+		NameCode{Type: "OTHER", Code: "GAS_CANERIA", Name: "Gas por Cañería"},
+		NameCode{Type: "OTHER", Code: "GYM", Name: "GYM"},
+		NameCode{Type: "OTHER", Code: "INSTALACION_TV_CABLE", Name: "Instalación de TV cable"},
+		NameCode{Type: "OTHER", Code: "JACUZZI", Name: "Jacuzzi"},
+		NameCode{Type: "OTHER", Code: "JARDIN", Name: "Jardín"},
+		NameCode{Type: "OTHER", Code: "LAVADERO", Name: "Lavadero"},
+		NameCode{Type: "OTHER", Code: "LAVANDERIA", Name: "Lavandería"},
+		NameCode{Type: "OTHER", Code: "LINEA_BLANCA", Name: "Línea Blanca"},
+		NameCode{Type: "OTHER", Code: "LOSA_RADIANTE", Name: "Losa Radiante"},
+		NameCode{Type: "OTHER", Code: "PARRILLERO", Name: "Parrillero"},
+		NameCode{Type: "OTHER", Code: "PATIO", Name: "Patio"},
+		NameCode{Type: "OTHER", Code: "PISCINA", Name: "Piscina"},
+		NameCode{Type: "OTHER", Code: "PLACARD_EN_COCINA", Name: "Placard en cocina"},
+		NameCode{Type: "OTHER", Code: "PLACARD_EN_DORMITORIO", Name: "Placard en dormitorio"},
+		NameCode{Type: "OTHER", Code: "PLAYROOM", Name: "Playroom"},
+		NameCode{Type: "OTHER", Code: "PREVISION_A_A", Name: "Previsión A.A."},
+		NameCode{Type: "OTHER", Code: "SAUNA", Name: "Sauna"},
+		NameCode{Type: "OTHER", Code: "SOTANO", Name: "Sótano"},
+		NameCode{Type: "OTHER", Code: "TERRAZA", Name: "Terraza"},
+		NameCode{Type: "OTHER", Code: "TERRAZA_LAVADERO", Name: "Terraza Lavadero"},
+		NameCode{Type: "OTHER", Code: "VESTIDOR", Name: "Vestidor"},
+		NameCode{Type: "OTHER", Code: "WIFI", Name: "WiFi"}}}
 
 var CountryPropertyTypes = map[string][]NameCode{
-	ar.Code:[]NameCode{ 
-		NameCode{Code:"APARTMENTO", Name: "Departamento"},
-		NameCode{Code:"LOCAL_COMERCIAL", Name: "Local Comercial"},
-		NameCode{Code:"PROPIEDAD_HORIZONTAL", Name: "Propiedad Horizontal"},
-		NameCode{Code:"TERRENO", Name: "Terreno"},
-		NameCode{Code:"GALPON", Name: "Galpon"},
-		NameCode{Code:"CASA", Name: "Casa"}},
-	uy.Code: []NameCode{ 
-			NameCode{Code:"APARTMENTO", Name: "Apartamento"},
-			NameCode{Code:"LOCAL_COMERCIAL", Name: "Local Comercial"},
-			NameCode{Code:"PROPIEDAD_HORIZONTAL", Name: "Propiedad Horizontal"},
-			NameCode{Code:"TERRENO", Name: "Terreno"},
-			NameCode{Code:"GALPON", Name: "Galpon"},
-			NameCode{Code:"CASA", Name: "Casa"}}}
+	ar.Code: []NameCode{
+		NameCode{Code: "APARTMENTO", Name: "Departamento"},
+		NameCode{Code: "LOCAL_COMERCIAL", Name: "Local Comercial"},
+		NameCode{Code: "PROPIEDAD_HORIZONTAL", Name: "Propiedad Horizontal"},
+		NameCode{Code: "TERRENO", Name: "Terreno"},
+		NameCode{Code: "GALPON", Name: "Galpon"},
+		NameCode{Code: "CASA", Name: "Casa"}},
+	uy.Code: []NameCode{
+		NameCode{Code: "APARTMENTO", Name: "Apartamento"},
+		NameCode{Code: "LOCAL_COMERCIAL", Name: "Local Comercial"},
+		NameCode{Code: "PROPIEDAD_HORIZONTAL", Name: "Propiedad Horizontal"},
+		NameCode{Code: "TERRENO", Name: "Terreno"},
+		NameCode{Code: "GALPON", Name: "Galpon"},
+		NameCode{Code: "CASA", Name: "Casa"}}}
